3e12be0117e2947c4bb91efb79b7a59512483bdf
WILLing <wangle1988918@163.com>
2018-09-03 21:58:00 +0800

    Initial commit

---
+++ entity/src/main/java/com/facetofront/entity/Product.java
@@ -0,0 +1,20 @@
+package com.facetofront.entity;
+
+import java.math.BigDecimal;
+
+public class Product {
+
+    private String id;
+    private String name;
+    private BigDecimal threshold_amount;
+    private BigDecimal step_amount;
+    private Integer lock_term;
+    private String reward_rate;
+    private String status;
+    private String memo;
+    private String create_at;
+    private String create_user;
+    private String update_at;
+    private String update_user;
+
+}
